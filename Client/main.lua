43f98f679a477bbfa06ddf682de1ac1042f0e9d0
MobTV <mobhater02@gmail.com>
2019-07-13 13:02:49 -0400

    Fixed Auto-Heal not disabling

--- Client/main.lua
+++ Client/main.lua
@@ -131,7 +131,7 @@ AddEventHandler('bonefive:client:ForceLeaveBed', function()
     LeaveBed()
 end)
 ---Make Damage Persistant (Disable Auto-Heal)
-SetPlayerHealthRechargeMultiplier(PlayerId(), 0.0)
+SetPlayerHealthRechargeMultiplier(PlayerId(0), 0.0)
 
 Citizen.CreateThread(function()
     while true do
